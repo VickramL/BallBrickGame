25f566a8b211cfbcacb625a0f2f5a04b73e818f5
Vikaram <vikramstuart23@gmail.com>
2023-05-01 17:37:00 +0530

    Brick Ball

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner input = new Scanner(System.in);
+    static int ballCount = 3;
+    static int n;
     public static void main(String[] args) {
         System.out.print("Enter the dimension : ");
-        int n = input.nextInt();
+        n = input.nextInt();
         char[][] board = new char[n][n];
         for(int i = 0;i<n;i++){
             board[0][i] = 'W';
@@ -21,11 +23,12 @@ public class Main {
     public static void printBoard(char[][] board,int n){
         for(int i = 0;i<n;i++){
             for(int j = 0;j<n;j++){
-                System.out.print((Character.isAlphabetic(board[i][j])||Character.isDigit(board[i][j]))?
+                System.out.print((Character.isLetterOrDigit(board[i][j]))?
                         board[i][j]+" ":"  ");
             }
             System.out.println();
         }
+        System.out.println("Ball Count "+ballCount);
     }
     public static void level1(char[][] board, int n){
         while (true) {
@@ -33,6 +36,10 @@ public class Main {
             int row = input.nextInt();
             int col = input.nextInt();
             char brick = input.next().charAt(0);
+            if(row < 0 || col < 0 || row>=n || col >= n || board[row][col] != 0){
+                System.out.println("Invalid position !!!");
+                continue;
+            }
             board[row][col] = brick;
             System.out.print("Do you want to continue (y/n): ");
             if(input.next().charAt(0) == 'n') break;
@@ -42,126 +49,92 @@ public class Main {
 
     public static void level2(char[][] board, int n){
         level1(board,n);
-        int ballCount = 3;
-        System.out.println("Ball Count "+ballCount);
         play(board,n);
         printBoard(board,n);
     }
 
     public static void play(char[][] board, int n){
         int[] ballPosition = {n-1,n/2};
-        int ballCount = 3;
+        ballCount = 3;
         while(true){
+            if(ballCount == 0){
+                System.out.println("Game Over !");
+                return;
+            }
+            int ballRow = ballPosition[0];
+            int ballCol = ballPosition[1];
             System.out.print("Enter the direction in which the ball need to traverse : ");
             String choice = input.next();
-            if(choice.equals("st")){
-                int ballRow = ballPosition[0];
-                int ballCol = ballPosition[1];
-                while (true){
-                    if(ballCount == 0){
-                        System.out.println("GAME OVER !");
-                        return;
-                    }
-                    if(board[ballRow][ballCol] == 'W'){
-                        ballCount--;
-                        break;
-                    }
-                    else if(Character.isDigit(board[ballRow][ballCol])){
-                        if(board[ballRow][ballCol] == '1'){
-                            board[ballRow][ballCol] = ' ';
-                        }
-                        else board[ballRow][ballCol] = (char) (board[ballRow][ballCol] -1);
 
-                        if(checkBoard(board,n)){
-                            System.out.println("you win HURRAY");
-                            return;
-                        }
-                        break;
-                    }
-                    ballRow--;
-                }
-            }
-            else if(choice.equals("ld")){
-                int ballRow = ballPosition[0];
-                int ballCol = ballPosition[1];
-                while (true){
-                    if(ballCount == 0){
-                        System.out.println("GAME OVER !");
-                        return;
-                    }
-                    if(board[ballRow][ballCol] == 'W'){
-                        ballPosition[0] = n-1;
-                        ballPosition[1] = n/2;
-                        ballCount--;
-                        break;
-                    }
-                    else if(Character.isDigit(board[ballRow][ballCol])){
-                        if(board[ballRow][ballCol] == '1'){
-                            board[ballRow][ballCol] = ' ';
-                            board[ballPosition[0]][ballPosition[1]] = 'G';
-                            ballPosition[0] = n-1;
-                            ballPosition[1] = ballCol;
-                            board[n-1][ballCol] = 'o';
-                            break;
-                        }
-                        else board[ballRow][ballCol] = (char) (board[ballRow][ballCol] -1);
-
-                        if(checkBoard(board,n)){
-                            System.out.println("you win HURRAY");
-                            return;
-                        }
-
-                    }
-                    ballRow--;
-                    ballCol--;
-                }
-            }
-
-            else{
-                int ballRow = ballPosition[0];
-                int ballCol = ballPosition[1];
-                while (true){
-                    if(ballCount == 0){
-                        System.out.println("GAME OVER !");
-                        return;
-                    }
-                    if(board[ballRow][ballCol] == 'W'){
-                        ballPosition[0] = n-1;
-                        ballPosition[1] = n/2;
-                        ballCount--;
-                        break;
-                    }
-                    else if(Character.isDigit(board[ballRow][ballCol])){
-                        if(board[ballRow][ballCol] == '1'){
-                            board[ballRow][ballCol] = ' ';
-                            board[ballPosition[0]][ballPosition[1]] = 'G';
-                            ballPosition[0] = n-1;
-                            ballPosition[1] = ballCol;
-                            board[n-1][ballCol] = 'o';
-                            break;
-                        }
-                        else board[ballRow][ballCol] = (char) (board[ballRow][ballCol] -1);
-
-                        if(checkBoard(board,n)){
-                            System.out.println("you win HURRAY");
-                            return;
-                        }
-
-                    }
-                    ballRow--;
-                    ballCol++;
-                }
-            }
+            move(board,ballRow,ballCol,ballPosition,choice);
+            printBoard(board,n);
         }
     }
 
     public static boolean checkBoard(char[][] board, int n){
         for(int i = 1;i<n-1;i++){
             for(int j = 1;j<n-1;j++){
-                if(Character.isDigit(board[i][j]))
+                if(Character.isLetterOrDigit(board[i][j]))
                     return false;
             }
         }
         return true;
+    }
+
+    public static void move(char[][] board, int ballRow, int ballCol,int[] ballPosition,String choice){
+        while (true){
+            if(board[ballRow][ballCol] == 'W'){
+                board[ballPosition[0]][ballPosition[1]] = 'G';
+                ballPosition[0] = n-1;
+                ballPosition[1] = n/2;
+                board[ballPosition[0]][ballPosition[1]] = 'o';
+                ballCount--;
+                return;
+            }
+            else if(Character.isDigit(board[ballRow][ballCol])){
+                if(board[ballRow][ballCol] == '1'){
+                    board[ballRow][ballCol] = ' ';
+                }
+                else board[ballRow][ballCol] = (char) (board[ballRow][ballCol] -1);
+
+                break;
+            }
+            else if(ballRow!= n-1 && Character.isAlphabetic(board[ballRow][ballCol])){
+                isCharacter(board,ballRow,ballCol);
+                break;
+            }
+            ballRow--;
+            if(choice.equals("ld")) ballCol--;
+            else if(choice.equals("rd")) ballCol++;
+
+        }
+        board[ballPosition[0]][ballPosition[1]] = 'G';
+        ballPosition[0] = n-1;
+        ballPosition[1] = ballCol;
+        board[ballPosition[0]][ballPosition[1]] = 'o';
+        if(checkBoard(board,n)){
+            System.out.println("you win HURRAY");
+            printBoard(board,n);
+            System.exit(1);
+        }
+    }
+
+    public static void isCharacter(char[][] board, int ballRow,int ballCol){
+        if(board[ballRow][ballCol] == 'e'){
+            for(int i = 1;i<n-1;i++){
+                board[ballRow][i] = ' ';
+            }
+        }
+        else if(board[ballRow][ballCol] == 's'){
+            board[ballRow][ballCol] = ' ';
+            if(ballRow-1 != 0) board[ballRow-1][ballCol] = ' ';
+            if(ballCol-1 != 0) board[ballRow][ballCol-1] = ' ';
+            if(ballRow+1 != n-1) board[ballRow+1][ballCol] = ' ';
+            if(ballCol+1 != n-1) board[ballRow][ballCol+1] = ' ';
+        }
+        else if(board[ballRow][ballCol] == 'b'){
+            board[ballRow][ballCol] = ' ';
+            ballCount += 1;
+        }
     }
 }
